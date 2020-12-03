32a2b3c9a352486ea5d426c70209ededf35114e4
Htin Shar Kyaw <htinsharrkyaw@gmail.com>
2020-12-03 20:58:36 +0630

    finished Redirecting data

--- src/main/java/com/htinshar/demo/web/HomeController.java
+++ src/main/java/com/htinshar/demo/web/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.htinshar.demo.domain.User;
 
@@ -18,8 +19,13 @@ public class HomeController {
 	}
 	
 	@RequestMapping(value="/create",method = RequestMethod.POST)
-	public String processFormData(User user){
-		
+	public String processFormData(User user,RedirectAttributes attr){//RedirectAttributes try data for redirection
+		attr.addFlashAttribute("user",user);
+		return "redirect:/display";
+	}
+	
+	@RequestMapping(value="/display",method = RequestMethod.GET)
+	public String displayFormData(User user) {
 		return "result";
 	}
 }
